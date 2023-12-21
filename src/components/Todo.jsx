6bcc710222467395c7002aab0b3bb097452e5e72
deepak <dgusaiwal537@gmail.com>
2023-12-21 22:12:47 +0530

    update note

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -8,31 +8,67 @@ import { useTodo } from "../context/TodoProvider";
 const Todo = ({ todo }) => {
   const { id, todoTxt, completed } = todo;
   const { deleteTodo, completeTodo } = useTodo();
+
+  const handdleUpdate = () => {};
   return (
-    <div
-      className={`grid-item ${
-        completed ? "bg-green-400" : "bg-red-400"
-      } rounded py-1 px-2 flex justify-between`}
-    >
-      <h3 className={`font-bold ${completed ? "line-through":""}`}>{todoTxt}</h3>
-      <MdDeleteForever
-        className="ml-auto cursor-pointer hover:text-white"
-        size="1.5rem"
-        onClick={() => {
-          deleteTodo(id);
-        }}
-      />
-      <MdEditDocument
-        className="cursor-pointer hover:text-white"
-        size="1.5rem"
-      />
-      <MdIncompleteCircle
-        className="cursor-pointer hover:text-white"
-        size="1.5rem"
-        onClick={() => {
-          completed ? null : completeTodo(id);
-        }}
-      />
+    <div className="grid-item">
+
+      <div
+        className={` ${
+          completed ? "bg-green-400" : "bg-red-400"
+        } rounded py-1 px-2 flex justify-between`}
+      >
+        <h3 className={`font-bold ${completed ? "line-through" : ""}`}>
+          {todoTxt}
+        </h3>
+        <MdDeleteForever
+          className="ml-auto cursor-pointer hover:text-white"
+          size="1.5rem"
+          onClick={() => {
+            deleteTodo(id);
+          }}
+        />
+        <MdEditDocument
+          className="cursor-pointer hover:text-white"
+          size="1.5rem"
+          onClick={handdleUpdate}
+        />
+        <MdIncompleteCircle
+          className="cursor-pointer hover:text-white"
+          size="1.5rem"
+          onClick={() => {
+            completed ? null : completeTodo(id);
+          }}
+        />
+      </div>
+
+      <div
+        className={` ${
+          completed ? "bg-green-400" : "bg-blue-400"
+        } rounded py-1 px-2 flex justify-between`}
+      >
+        <input type="text" name="" className="rounded" id="" />
+        <MdDeleteForever
+          className="ml-auto cursor-pointer hover:text-white"
+          size="1.5rem"
+          onClick={() => {
+            deleteTodo(id);
+          }}
+        />
+        <MdEditDocument
+          className="cursor-pointer hover:text-white"
+          size="1.5rem"
+          onClick={handdleUpdate}
+        />
+        <MdIncompleteCircle
+          className="cursor-pointer hover:text-white"
+          size="1.5rem"
+          onClick={() => {
+            completed ? null : completeTodo(id);
+          }}
+        />
+      </div>
+
     </div>
   );
 };
